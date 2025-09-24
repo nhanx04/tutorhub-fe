3d2a004a1307098e69fa3f1c74e559694fd9be44
nhanx04 <nhan.nguyencs04@gmail.com>
2025-09-24 16:08:27 +0700

    add login page

---
+++ app/routes/login.tsx
@@ -0,0 +1,3 @@
+import LoginPage from '../../src/features/login'
+
+export default LoginPage
--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,5 +1,5 @@
 import { useContext } from 'react'
-import { AuthContext } from 'src/context'
+import { AuthContext } from '../context'
 
 export function useAuth() {
   const context = useContext(AuthContext)
